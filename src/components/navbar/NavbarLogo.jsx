3682bb1af11c3def8681eff4eca92dab4d416890
Vivek Kumar <viveksoni6280@gmail.com>
2025-08-02 17:57:18 +0530

    Fix image paths and update navbar text

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -5,7 +5,7 @@ const NavbarLogo = () => {
         Vivek Kumar
       </h1>
       <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-        AQ
+        Vk
       </h1>
     </div>
   );
