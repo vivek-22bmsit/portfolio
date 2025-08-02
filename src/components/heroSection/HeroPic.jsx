e3fb6dc285ba5b83a7d5087dca38465b903021cd
Vivek Kumar <viveksoni6280@gmail.com>
2025-08-02 17:48:22 +0530

    Fix image paths and update portfolio

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/HexaPic.png"
+        src="/images/HexaPic.png"
         alt="Vivek Kumar"
         className="max-h-[450px] w-auto"
       />
